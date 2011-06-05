1bd61d19c0716d1e7e9138520e101785599de604
Laszlo Kiss-Kollar <kiss.kollar.laszlo@gmail.com>
2011-06-05 11:42:34 +0200

    Completed functionality in datastore.py.

--- backend/datastore.py
+++ backend/datastore.py
@@ -1,5 +1,11 @@
 import sqlite3
 
+"""
+Schema:
+CREATE TABLE history(id integer primary key, title varchar, summary text, content text, editor varchar, date text, historyid integer);
+CREATE TABLE pages(id integer primary key, title varchar, summary text, content text, editor varchar, date text, historyid integer);
+"""
+
 class SQLiteDataStore(object):
     
     def __init__(self):
@@ -7,9 +13,45 @@ class SQLiteDataStore(object):
         self.connection = sqlite3.connect("innowiki.sqlite")
         self.c = self.connection.cursor()
 
-    def addPage(self, title, summary, content, editor, date, history_id):
+
+    def addActivePage(self, title, summary, content, editor, date, history_id):
+        #TODO error handling
         self.c.execute("insert into pages values(NULL,?,?,?,?,?,?)", (title, summary, content, editor, date, history_id,))
         self.connection.commit()
+
+    def getActivePage(self, title):
+        self.c.execute("select * from pages where title=(?)", (title,))
+        return self.c.fetchone()
+
+    def getActivePageList(self):
+        r = []
+        self.c.execute("select id, title, summary, editor, date from pages order by id")
+        for row in self.c:
+            r.append(row)
+
+        return r
+
+    def addHistoryPage(self, title, summary, content, editor, date, history_id):
+        self.c.execute("insert into history values(NULL,?,?,?,?,?,?)", (title, summary, content, editor, date, history_id,))
+        self.connection.commit()
+
+    def getHistoryPage(self, title):
+        self.c.execute("select * from history where title=(?)", (title,))
+        return self.c.fetchone()
+
+    def getLastHistoryId(self):
+        self.c.execute("select * from history order by id desc")
+        row = self.c.fetchone()
+        return row[0]
+
+    def getHistoryPageListNewerThan(self, id):
+        r = []
+        self.c.execute("select id, title, summary, editor, date from pages where id > (?)", (id,))
+        for row in self.c:
+            r.append(row)
+
+        return r
+
 
     def __del__(self):
         self.c.close()
